035cde5070c4424061592ba2caa6483b29bcd759
dayvesoliveira <dayvesoliveira@gmail.com>
2017-06-13 00:07:06 -0300

    Primeiro comit da arquit. proposta.

---
+++ client/app/js/app.js
@@ -0,0 +1,6 @@
+import angular from 'angular';
+import 'angular-route';
+
+//import CustomersCtrl from './controllers/customers-ctrl';
+
+var app = angular.module('AppDribble', ['ngRoute']);
---
+++ client/webpack.config.js
@@ -0,0 +1,15 @@
+var path = require('path');
+module.exports = {
+    entry: "./index.js",
+    devtool: 'source-map',
+    output: {
+        path: path.resolve(__dirname ,'client'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader'
+        }]
+    }
+}
